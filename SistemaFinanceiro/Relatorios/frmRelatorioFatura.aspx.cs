cc03b0caadfdfded633b0b9fce1098542ecd7815
Leonardo <leozinhoo013@gmail.com>
2018-10-20 17:57:43 -0300

    primeiro comentario

---
+++ SistemaFinanceiro/Relatorios/frmRelatorioFatura.aspx.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SistemaFinanceiro.Relatorios
+{
+    public partial class frmRelatorioFatura : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand comando;
+        SqlDataAdapter adapter;
+        SqlParameter param;
+        string idVenda;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source=HUGO-PC\\HUGO;Initial Catalog=financeiro;Integrated Security=True");
+            if (!IsPostBack)
+            {
+                renderReport();
+
+            }
+        }
+
+        public void renderReport()
+        {
+            idVenda = Request.QueryString.Get("idVenda");
+
+            DataTable dt = carregar(idVenda);
+            ReportDataSource rds = new ReportDataSource("DataSet1", dt);
+            ReportViewer1.LocalReport.DataSources.Add(rds);
+            ReportViewer1.LocalReport.ReportPath = "Relatorios/frmRelatorioFatura.rdlc";
+
+            //parameters
+            ReportParameter[] rptParams = new ReportParameter[]
+            {
+                new ReportParameter("idVenda",idVenda.ToString())
+        };
+            ReportViewer1.LocalReport.Refresh();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public DataTable carregar(string codigovenda)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection cn = new SqlConnection("Data Source=HUGO-PC\\HUGO;Initial Catalog=financeiro;Integrated Security=True"))
+            {
+
+                SqlCommand cmd = new SqlCommand("sp_relatorio_venda", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@idVenda", SqlDbType.Int).Value = idVenda;
+
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                adp.Fill(dt);
+            }
+            return dt;
+
+        }
+    }
+}
---
+++ SistemaFinanceiro/Relatorios/frmRelatorioProduto.aspx.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaFinanceiro.Relatorios
+{
+    public partial class frmRelatorioProduto : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand comando;
+        SqlDataAdapter adapter;
+        SqlParameter param;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source=HUGO-PC\\HUGO;Initial Catalog=financeiro;Integrated Security=True");
+            if (!IsPostBack)
+            {
+                renderReport();
+
+            }
+        }
+
+
+        public void renderReport()
+        {
+          
+
+            DataTable dt = carregar();
+            ReportDataSource rds = new ReportDataSource("DataSet1", dt);
+            ReportViewer1.LocalReport.DataSources.Add(rds);
+            ReportViewer1.LocalReport.ReportPath = "Relatorios/frmRelatorioProduto.rdlc";
+
+          
+            ReportViewer1.LocalReport.Refresh();
+
+        }
+
+       
+
+        public DataTable carregar()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection cn = new SqlConnection("Data Source=HUGO-PC\\HUGO;Initial Catalog=financeiro;Integrated Security=True"))
+            {
+
+                SqlCommand cmd = new SqlCommand("select * from produto order by nome asc", con);
+                cmd.CommandType = CommandType.Text;
+                
+
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                adp.Fill(dt);
+            }
+            return dt;
+
+        }
+    }
+
+}
